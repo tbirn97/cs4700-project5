cfd341947a9e119b711c909ad66bd4e28e5662cd
Tyler Birn <tylerbirn@Tylers-MacBook-Pro.local>
2020-04-15 15:48:04 -0400

    initial changes

--- httpserver.py
+++ httpserver.py
@@ -1,30 +1,38 @@
 import argparse
 import http.server
 import socketserver
+import urllib
+
+global DATA
 
 
-class HTTPServer:
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
-    def __init__(self, port, origin):
-        self.port = port
-        self.origin = origin
-        self.server_handler = http.server.SimpleHTTPRequestHandler
+    def __pull_index_file(self):
+        global DATA
+        try:
+            file_listener = urllib.URLopener()
+            DATA = file_listener.retrieve("http://3.88.208.124/index.html", "test.html")
+        except IOError:
+            return None
 
-    def begin(self):
-        print("setting up server...")
-        http_serv = socketserver.TCPServer(("", self.port), self.server_handler)
-        print("server set up. preparing to listen...")
-        # TODO: Do we need to check if index exists, or can we assume it does
-        # TODO: Do we assume that this server has the file, or should we go get it.
-        http_serv.serve_forever()
-
-
-
-
-
-
+    def do_GET(self):
+        global DATA
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        if not DATA:
+            self.__pull_index_file()
+        self.wfile.write(bytes(DATA))
 
 
+def main(port, origin):
+    print("setting up server...")
+    server_handler = http.server.SimpleHTTPRequestHandler
+    http_serv = socketserver.TCPServer(("", port), server_handler)
+    print("server set up. Checking for file...")
+    print("Got file. Preparing to listen...")
+    http_serv.serve_forever()
 
 
 if __name__ == '__main__':
@@ -32,5 +40,5 @@ if __name__ == '__main__':
     parser.add_argument('-p', dest='port', type=str, help='The port on which to bind the HTTP server')
     parser.add_argument('-o', dest='origin', type=int, help='The origin where the CSN is')
     args = parser.parse_args()
-    server = HTTPServer(args.port, args.origin)
+    main(args.port, args.origin)
 
